7c840f8731cec4eaf3530a69239304c7d99a1ed8
charan96 <charan.sudarsanam@gmail.com>
2016-03-09 17:34:47 -0800

    added more elements to card and game

--- src/main/java/models/Card.java
+++ src/main/java/models/Card.java
@@ -1,8 +1,35 @@
 package models;
 
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 /**
  * Created by ramcharan on 3/9/16.
  */
 public class Card
 {
+    public final int value;
+    public final Suit suit;
+
+    @JsonCreator
+    public Card(@JsonProperty("value") int value, @JsonProperty("suit") Suit suit)
+    {
+        this.value = value;
+        this.suit = suit;
+    }
+
+    public suit getSuit()
+    {
+        return this.suit;
+    }
+
+    public int getValue()
+    {
+        return this.value;
+    }
+
+    public String toString()
+    {
+        return this.value + this.suit.toString();
+    }
 }
